c195c1a7082058dc1358addfaecd5448fef6aa2a
Sanjeev Kumar <sanjeevf800@gmail.com>
2026-04-14 19:54:39 +0530

    no message

--- src/main/java/com/springboot3/restapis/config/Config.java
+++ src/main/java/com/springboot3/restapis/config/Config.java
@@ -2,18 +2,15 @@ package com.springboot3.restapis.config;
 
 
 import com.springboot3.restapis.config.message.MessageService;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "books")
 public class Config {
 
     @Bean
     public MessageService messageService() {
         return new MessageService();
     }
-
 
 }
